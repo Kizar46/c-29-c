aaedf10a0b8c1de08a61640502f1983fbdf996d3
Kiser46 <kizarhussain477@gmail.com>
2021-05-22 21:09:15 +0530

    30

--- sketch.js
+++ sketch.js
@@ -16,7 +16,7 @@ function setup(){
     world = engine.world;
 
     
-    ground = new Ground(600,height,1200,20)
+    ground = new Ground(600,height,1200,30)
 
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
@@ -29,10 +29,11 @@ function setup(){
 
     log3 =  new Log(810,180,300, PI/2);
 
-    
+
     box5 = new Box(810,160,70,70);
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
+
 
     bird = new Bird(100,100);
     platform1=new Ground(100,300,300,180)
